2b745791234c9c9003b857971a4cd718c1e03429
brunannunes <brunanunes324@gmail.com>
2024-06-05 16:27:54 -0300

    falta mt coisa

--- Front/src/Componentes/Cabecalho.jsx
+++ Front/src/Componentes/Cabecalho.jsx
@@ -1,10 +1,35 @@
-import estilos from './Cabecalho.module.css'
+import styles from './Cabecalho.module.css'
+import { Link } from 'react-router-dom';
 
 
 export function Cabecalho(){
     return(
-        <header className={estilos.conteiner}>            
-            <p>SmartCity</p>            
+        <header className={styles.container}>            
+            <h1 className={styles.titulo}>SmartCity</h1>            
+    
+            <Link 
+                className={styles.link}
+                to='/inicial'>
+                Lista Sensores
+            </Link>
+
+            <Link 
+
+                className={styles.link}
+                to='/inicial/cadsensor'
+            >Cadastrar Sensor
+            </Link>
+
+            <Link 
+                className={styles.link}
+                to='/inicial/localizacao'
+            >Mapa
+            </Link>
+
+            <Link 
+            className={styles.link}
+            to='/'
+            >Log Out</Link>
         </header>
-    )
+    );
 }
